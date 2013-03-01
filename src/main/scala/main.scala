08d0e4da52b3eec394507402df8f00526ef94bcb
Francesco Strozzi <francesco.strozzi@gmail.com>
2013-03-01 22:16:21 +0100

    working version without Redis

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,16 +1,24 @@
 
-import com.top10.redis._
+//import com.top10.redis._
 import java.util.zip._
 import java.io._
 import scala.io._
 
 object VcfCompare extends App {
-  
-  val redis = new SingleRedis("localhost",6379)
+ 
+  val dict = Map("AA"->1,"AT"->2,"AC"->3,"AG"->4,
+                  "TA"->5,"TC"->6,"TG"->7,"TT"->8,
+                  "CA"->9,"CT"->10,"CG"->11,"CC"->12,
+                  "GA"->13,"GT"->14,"GC"->15,"GG"->16
+    )
+  //val redis = new SingleRedis("localhost",6379)
   val maxSamples = args.size
+  val vcf = new java.util.HashMap[String,Array[Int]]()
+
   args.zipWithIndex.foreach {a =>
-    val vcf = openFile(a._1)
-    addVcf(vcf,a._2,maxSamples,redis)
+    val vcfFile = openFile(a._1)
+    addVcf(vcf,vcfFile,a._2,maxSamples)
+    println(vcf.size)
   } 
 
   def openFile(vcf: String) : BufferedSource = {
@@ -24,29 +32,27 @@ object VcfCompare extends App {
   }
 
 
-  def addVcf(openVcf: BufferedSource, samplePos: Int, maxSamples: Int, redis: SingleRedis) = { 
-    var vcf = Set[String]()
-    redis.exec(pipeline => {
+  def addVcf(vcf: java.util.HashMap[String,Array[Int]],openVcf: BufferedSource, samplePos: Int, maxSamples: Int) = { 
       openVcf.getLines.foreach {line =>  
         if(!line.startsWith("#")) {
           val elements = line.split("\t")
-          val key = elements(0)+"-"+elements(1)+"-"+elements(3)
-          val value = redis.get(key)
-          value match {
-            case Some(x) => {
-              val alternatives = x.updated(samplePos,elements(4))
-              pipeline.set(key,alternatives.toString)
-            } 
-            case None => {
-              val alternatives = new String(Array.fill[Char](samplePos)('0') ++ elements(4) ++
-                               Array.fill[Char](maxSamples-(samplePos+1))('0'))
-              pipeline.set(key,alternatives)
-            } 
+          val key = elements(0)+"-"+elements(1)
+          val value = vcf.get(key)
+          var alleles = elements(4).split(',').reduceLeft((a,b)=>a+b)
+          if (alleles.size == 1) {
+            alleles = elements(3)+alleles  
           }
-         }
+          value match {
+            case x: Array[Int] => {
+              x(samplePos) = dict(alleles)
+              vcf.put(key,x)
+            }
+            case null => {
+              vcf.put(key,(Array.fill[Int](samplePos)(0)++Array(dict(alleles))++Array.fill[Int](maxSamples-(samplePos+1))(0)))
+            }
+          }
         }
-      })  
-      vcf 
+      }
     }
   }
 
